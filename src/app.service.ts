d58a1e65579b5ef7f90d733c51d1e4ff5edabf40
seung <skyhs3507@likelion.org>
2024-01-09 22:06:27 +0900

    feat: split spam filter job into functions

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@nestjs/common';
 import { SpamQueryDto } from './dto/spam.dto';
+import axios from 'axios';
+import { JSDOM } from 'jsdom';
 
 @Injectable()
 export class AppService {
   async isSpam(spamQueryDto: SpamQueryDto): Promise<boolean> {
+    const { content, spamLinkDomains, redirectionDepth } = spamQueryDto;
+
+    const urls = this.extractURLsFromContent(content);
+
+    for (const url of urls) {
+      const finalUrl = await this.followRedirections(url, redirectionDepth);
+      if (this.checkIfSpam(finalUrl, spamLinkDomains)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  private extractURLsFromContent(content: string): RegExpMatchArray | [] {
+    return content.match(/https?:\/\/[^ ]+/g) ?? [];
+  }
+
+  private checkIfSpam(url: string, spamLinkDomains: string[]): boolean {
     return true;
+  }
+
+  private getDomain(url: string): string {
+    return '';
+  }
+
+  private async followRedirections(
+    url: string,
+    depth: number,
+  ): Promise<string> {
+    return '';
   }
 }
