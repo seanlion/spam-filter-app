8badfe9dc1eeb5529d1a8c12f5ae70743db12ec7
seung <skyhs3507@likelion.org>
2024-01-09 21:27:54 +0900

    init a project

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV === 'dev' || process.env.NODE_ENV === 'staging'
+          ? './.env'
+          : './.prod.env',
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
